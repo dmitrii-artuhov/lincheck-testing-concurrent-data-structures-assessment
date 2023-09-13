4f97222c48d2888b047754b1cb12f56d0bdf829b
Dmitrii Art <artuhovdmitrii@gmail.com>
2023-09-13 21:41:19 +0200

    Implement the Michael-Scott lock-free queue

--- src/MSQueue.kt
+++ src/MSQueue.kt
@@ -1,15 +1,67 @@
 import java.util.concurrent.atomic.AtomicReference
 
+
+// Note: no memory-allocation ABA problem possible in PL with garbage collection,
+// so here no need to implement counters as stated in the paper (http://www.cs.rochester.edu/~scott/papers/1996_PODC_queues.pdf)
+// explanation is here: https://stackoverflow.com/questions/42854116/why-does-automatic-garbage-collection-eliminate-aba-problems
+// So we don't need `AtomicStampedReference` class because we are running CAS on `Node` addresses and not on `Node::element` values
+
 class MSQueue<E> : Queue<E> {
     private val head: AtomicReference<Node<E>>
     private val tail: AtomicReference<Node<E>>
 
+    init {
+        val dummyNode = Node<E>(null)
+
+        head = AtomicReference(dummyNode)
+        tail = AtomicReference(dummyNode)
+    }
+
     override fun enqueue(element: E) {
-        TODO("Implement me")
+        val newNode: Node<E> = Node(element)
+
+        while (true) {
+            val currentTail: Node<E> = tail.get()
+            val tailNext: Node<E>? = currentTail.next.get()
+
+            if (currentTail == tail.get()) {
+                if (tailNext != null) {
+                    tail.compareAndSet(currentTail, tailNext)
+                }
+                else {
+                    if (currentTail.next.compareAndSet(null, newNode)) {
+                        tail.compareAndSet(currentTail, newNode)
+                        return
+                    }
+                }
+            }
+        }
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        while (true) {
+            val first: Node<E> = head.get()
+            val last: Node<E> = tail.get()
+            val next: Node<E>? = first.next.get()
+
+            if (first == head.get()) {
+                if (first == last) {
+                    if (next == null) {
+                        return null
+                    }
+                    tail.compareAndSet(last, next)
+                }
+                else {
+                    val result: E? = next?.element
+                    if (head.compareAndSet(first, next)) {
+                        if (result != null) {
+                            next.element = null
+                        }
+                        return result
+                    }
+                }
+            }
+        }
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
